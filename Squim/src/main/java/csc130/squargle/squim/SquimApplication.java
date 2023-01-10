f3cf26c9f327f3cf605eb562991792233b1f2ddf
Alexis Seals <blarfy6216@gmail.com>
2023-01-10 14:08:17 -0700

    Setup
    Creatin Squim

---
+++ Squim/src/main/java/csc130/squargle/squim/SquimApplication.java
@@ -0,0 +1,13 @@
+package csc130.squargle.squim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquimApplication.class, args);
+    }
+
+}
