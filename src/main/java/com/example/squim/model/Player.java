d3b9924010509a1c9da7e2b26c83d1332367d968
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2023-01-18 14:12:49 -0700

    made player class

--- src/main/java/com/example/squim/model/Player.java
+++ src/main/java/com/example/squim/model/Player.java
@@ -1,4 +1,22 @@
 package com.example.squim.model;
 
 public class Player {
+    private boolean isHuman;
+    private String name;
+
+    public boolean isHuman() {
+        return isHuman;
+    }
+
+    public void setHuman(boolean human) {
+        isHuman = human;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
