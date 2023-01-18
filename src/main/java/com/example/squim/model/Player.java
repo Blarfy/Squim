f596e1aac3a6a7ddeeff3ca554745dbcc855bbdc
Alexis Seals <blarfy6216@gmail.com>
2023-01-18 14:23:38 -0700

    Update Player.java

--- src/main/java/com/example/squim/model/Player.java
+++ src/main/java/com/example/squim/model/Player.java
@@ -1,4 +1,35 @@
 package com.example.squim.model;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    private boolean isHuman;
+    private String name;
+
+    public boolean getIsHuman() {
+        return isHuman;
+    }
+
+    public void setIsHuman(boolean human) {
+        isHuman = human;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Row> takeTurn(ArrayList<Row> board) {
+        boolean oddOnes = false;
+        boolean oddTwos = false;
+        boolean oddFours = false;
+        for (Row row : board) {
+            
+        }
+        return board;
+    }
 }
