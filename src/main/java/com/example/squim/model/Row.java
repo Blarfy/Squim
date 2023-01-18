d3ef0895c4e004d5dd8ae37b01ff0bb9bf862d69
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2023-01-18 14:21:18 -0700

    made row class

--- src/main/java/com/example/squim/model/Row.java
+++ src/main/java/com/example/squim/model/Row.java
@@ -1,4 +1,39 @@
 package com.example.squim.model;
 
+import java.util.ArrayList;
+
 public class Row {
+    private int count;
+    private ArrayList<Boolean> rocks;
+
+    public Row(int count) {
+        this.count = count;
+        rocks = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            rocks.add(true);
+        }
+
+    }
+
+    public boolean deleteRock(int rock){
+        rocks.remove(rock);
+        return true;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public ArrayList<Boolean> getRocks() {
+        return rocks;
+    }
+
+    public void setRocks(ArrayList<Boolean> rocks) {
+        this.rocks = rocks;
+    }
 }
