698ab0fbc28dc3eb7f2b369e5824f02cad853340
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2023-01-23 13:37:01 -0700

    gamecomtrollerstuff

--- src/main/java/com/example/squim/controller/GameController.java
+++ src/main/java/com/example/squim/controller/GameController.java
@@ -1,13 +1,37 @@
 package com.example.squim.controller;
 
 import com.example.squim.model.Game;
+import com.example.squim.model.Player;
 
 import java.util.ArrayList;
 
 public class GameController {
-    private ArrayList<Game> games;
+    private static ArrayList<Game> games;
 
     public GameController() {
         games = new ArrayList<>();
     }
+
+    public static void createGame(int id, Player[] players){
+        Game game = new Game(id, players);
+        games.add(game);
+    }
+
+    public static void updateGame(){
+
+    }
+
+    private static void deleteGame(){
+
+    }
+
+    private static Game findGame(int id){
+        for (int i = 0; i < games.size(); i++) {
+            if(games.get(i).getId()==id){
+                return games.get(i);
+            }
+        }
+        
+    }
+
 }
