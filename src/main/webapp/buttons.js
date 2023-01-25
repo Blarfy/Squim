9287a54ed1d7c02c2a37ec999dac3459e8d87d7f
Alex <mcbuzzer@gmail.com>
2023-01-24 19:02:21 -0700

    Reorganized files

--- src/main/webapp/buttons.js
+++ src/main/webapp/buttons.js
@@ -93,34 +93,3 @@ hardDK.onclick = () => {
 //         body: document.cookie// GAME COOKIE
 //     });
 // }
-
-// const startGame = () => {
-//     game = {
-//         "gameId": 1,
-//         "rows": [
-//                     [true],
-//                     [true,true,true],
-//                     [true,true,true,true,true],
-//                     [true,true,true,true,true,true,true]
-//                 ],
-//         "players": ["Player 1", "Player 2"],
-//     }
-//     for (let index = 0; index < game.rows.length; index++) {
-//         let string = "r" + (index + 1)
-//         for (let jindex = 0; jindex < game.rows[index].length; jindex++) {
-//             let num = getRandomInt(1, 7);
-//             let dwayne = `<div class="Dwayne" id="${index}-${jindex}"><img src="resources/Dwayne${num}.png" /></div>`;
-//             document.getElementById(`r${index+1}`).innerHTML += dwayne;
-//         }
-        
-        
-//     }
-// }
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min) + min);
-// }
-
-// window.onload = () => {
-//     startGame();
-// }
