3b6674cc749b604a2b8e166bdb4bcece37a35550
Alex <mcbuzzer@gmail.com>
2023-01-25 12:35:14 -0700

    Nim is done, at least the frontend

--- src/main/webapp/helpButton.js
+++ src/main/webapp/helpButton.js
@@ -2,7 +2,7 @@ let popupFunction = () => {
     // Useful GitHub Copilot suggestion
     // let popup = document.getElementById("popup");
     // popup.classList.toggle("show");
-    alert("Players take turns removing objects from the piles. On each turn, a player must remove at least one object, and may remove any number of objects provided they all come from the same pile. The goal of the game is to be the player to remove the last object. The player who removes the last object loses the game. Players should strategize and plan their moves in order to force their opponent to take the last object.");
+    alert("Click on a Dwayne to select him for removal. You can only select up to 2 Dwaynes per turn. Click the 'End Turn' button to remove the selected Dwaynes. Players take turns doing this, the game ends when all Dwaynes are removed. Good luck!");
 }
 
 function getRandomInt(min, max) {
