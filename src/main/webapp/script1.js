f08ff8eaaed57296ac1bc03ec819080a78fec9b9
Alex <mcbuzzer@gmail.com>
2023-01-24 18:41:00 -0700

    Option Page added

--- src/main/webapp/script1.js
+++ src/main/webapp/script1.js
@@ -1,6 +1,10 @@
-let popupFunction = () => {
+const popupFunction = () => {
     // Useful GitHub Copilot suggestion
     // let popup = document.getElementById("popup");
     // popup.classList.toggle("show");
     alert("Hello World! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam enim eos dolor iusto, in minus quasi consectetur qui nobis voluptatum necessitatibus saepe. In eligendi, cumque eaque maiores molestiae quod deleniti? This will be replaced with a real in-page popup");
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
